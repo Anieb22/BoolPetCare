090c5715a08239603e3ae227e2be34c9008726b4
S-Lombardi <silo.lombardi@gmail.com>
2023-08-22 22:54:17 +0200

    iniziato file create

---
+++ resources/views/admin/animals/create.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.admin')
+
+{{-- FORM PER INSERIRE NUOVI PAZIENTI ANIMALI :3 --}}
+@section('content')
+    <div class="container">
+
+        <h4 class='mt-4'>Compila la form e aggiungi un paziente peloso, piumato o rugoso</h4>
+
+    </div>
+@endsection
