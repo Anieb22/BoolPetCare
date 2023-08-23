17c510b55ba963d7d28ae05f49758994954618df
Anieb22 <anielloverdino@gmail.com>
2023-08-23 14:34:24 +0200

    creato store (da definire la grafica)

--- app/Http/Controllers/admin/AnimalController.php
+++ app/Http/Controllers/admin/AnimalController.php
@@ -49,7 +49,7 @@ class AnimalController extends Controller
      */
     public function show(Animal $animal)
     {
-        //
+        return view('admin.animals.show', compact('animal'));
     }
 
     /**
