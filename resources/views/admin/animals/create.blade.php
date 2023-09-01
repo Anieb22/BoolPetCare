8b4ca6bde402c214f25283ec6b7c62edba9ad577
quintusdev <126263354+quintusdev@users.noreply.github.com>
2023-09-01 22:39:46 +0200

    [FEATURES][UPDATE] Inseriti elementi in show e aggiunto campi form info

--- resources/views/admin/animals/create.blade.php
+++ resources/views/admin/animals/create.blade.php
@@ -18,43 +18,44 @@
                     </div>
                 @endif
 
-                
-                <form action="{{route('admin.animals.store')}}" method="POST">
+
+                <form action="{{ route('admin.animals.store') }}" method="POST">
                     @csrf
-                    
+
                     <div class="form-group">
                         <label for="" class="control-label">Nome</label>
                         <input type="text"class="form-control" id="name" name="name">
                     </div>
 
-                    
+
                     <div class="form-group">
                         <label for="" class="control-label">Specie</label>
                         <input type="text"class="form-control" id="specie" name="specie">
                     </div>
 
-                    
+
                     <div class="form-group">
                         <label for="" class="control-label">Data di nascita</label>
                         <input type="date"class="form-control" id="date_of_birth" name="date_of_birth">
                     </div>
 
-                    
+
                     <div class="form-group">
                         <label for="" class="control-label">Genere</label>
-                        <select class="form-select form-select-sm" aria-label="Small select example" id="genre" name="genre">
-                          <option selected value="1">Maschio</option>
-                          <option value="0">Femmina</option>
+                        <select class="form-select form-select-sm" aria-label="Small select example" id="genre"
+                            name="genre">
+                            <option selected value="Maschio">Maschio</option>
+                            <option value="Femmina">Femmina</option>
                         </select>
                     </div>
 
-                    
+
                     <div class="form-group">
                         <label for="" class="control-label">Nome del proprietario</label>
                         <input type="text"class="form-control" id="owner" name="owner">
                     </div>
 
-                    
+
                     <div class="form-group mb-3">
                         <label for="" class="control-label">Note aggiuntive</label>
                         <textarea type="text"class="form-control" id="note" name="note"></textarea>
@@ -62,24 +63,25 @@
 
                     <div class="form-group mb-3">
                         <div>Seleziona la vaccinazione</div>
-                            <select class="form-control" name="vaccination[]">
-                                <option value="">Seleziona una tipologia di vaccino</option>
-                                
-                                @foreach($vaccinations as $vaccination)
-                                    <option {{ $vaccination->id }} value="{{ $vaccination->id }}">{{ $vaccination->type }}</option>
-                                @endforeach
-                            </select>
-                        </div>
-                    </div>
+                        <select class="form-control" name="vaccination[]">
+                            <option value="">Seleziona una tipologia di vaccino</option>
 
-                    <div class="col-12 d-flex justify-content-between">
-                        <a class="btn btn-md btn-primary" href="{{route('admin.animals.index')}}">Torna alla home</a>
-                        <div class="form-group">
-                            <button class="btn btn-sm btn-success" type="submit">Salva</button>
-                        </div>
+                            @foreach ($vaccinations as $vaccination)
+                                <option {{ $vaccination->id }} value="{{ $vaccination->id }}">{{ $vaccination->type }}
+                                </option>
+                            @endforeach
+                        </select>
                     </div>
-                </form>
             </div>
+
+            <div class="col-12 d-flex justify-content-between">
+                <a class="btn btn-md btn-primary" href="{{ route('admin.animals.index') }}">Torna alla home</a>
+                <div class="form-group">
+                    <button class="btn btn-sm btn-success" type="submit">Salva</button>
+                </div>
+            </div>
+            </form>
         </div>
+    </div>
     </div>
 @endsection
--- resources/views/admin/animals/show.blade.php
+++ resources/views/admin/animals/show.blade.php
@@ -11,10 +11,12 @@
                     class="col-8 bg-success d-flex flex-row mt-4 align-items-center border rounded border-0 p-2 justify-content-between">
                     <section class="d-flex flex-row">
                         <h3 class="text-white bold-text m-4"> Nome del tuo Pet:
-                            <h2 class="text-white bold-text"><strong>{{ $animal->name }}</strong></h2>
+                            <h2 class="text-white bold-text m-4"><strong>{{ $animal->name }}</strong></h2>
                         </h3>
                     </section>
                 </div>
+
+                <img src="{{ URL::asset('/image/propic.png') }}" alt="profile Pic" height="200" width="200">
                 <div class="col-12 d-flex flex-row justify-content-between">
                     <div class="col-8 my-4">
                         <h4 class="text-black">Specie del tuo Pet: {{ $animal->specie }}</h4>
@@ -86,5 +88,51 @@
                 </div>
             </div>
         </div>
+        {{-- SEZIONI ANIMALI VISITATI --}}
+        <section class="counter">
+            <div class="container">
+                <div class="counter__content mt-5">
+                    <div class="row">
+                        <div class="col-lg-3 col-md-6 col-sm-6">
+                            <div class="counter__item">
+                                <div class="counter__item__text">
+                                    <i class="icon fas fa-project-diagram fa-3x"></i>
+                                    <h2 class="counter_num">54</h2>
+                                    <p>Animali Inseriti ogni giorno
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-3 col-md-6 col-sm-6">
+                            <div class="counter__item second__item">
+                                <div class="counter__item__text">
+                                    <i class="icon far fa-smile-beam fa-3x"></i>
+                                    <h2 class="counter_num">452</h2>
+                                    <p>Proprietari felici</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-3 col-md-6 col-sm-6">
+                            <div class="counter__item third__item">
+                                <div class="counter__item__text">
+                                    <i class="icon fas fa-chalkboard-teacher fa-3x"></i>
+                                    <h2 class="counter_num">57</h2>
+                                    <p>Nuovi Pet ogni giorno</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-3 col-md-6 col-sm-6">
+                            <div class="counter__item four__item">
+                                <div class="counter__item__text">
+                                    <i class="icon fas fa-list-alt fa-3x"></i>
+                                    <h2 class="counter_num">5418</h2>
+                                    <p>Per visitati ogni anno</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
     </div>
 @endsection
