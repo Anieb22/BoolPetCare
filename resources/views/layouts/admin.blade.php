93c43b375b494f11c2de803bb167e306a538b119
Anieb22 <anielloverdino@gmail.com>
2023-09-03 14:03:23 +0200

    aggiunta responsività alla navbar anche ad admin

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -23,60 +23,54 @@
 
 
     <nav class="navbar navbar-expand-lg bg-dark">
-        <div class="container-fluid container">
-          <div class="col-lg-2 col-3 col-xl-1">
-              <a class="navbar-brand" href="{{ url('/') }}">
-                  <img src="{{ Vite::asset('public\img\BoolPetCare@4x.png')}}" alt=""  id="logo">
-              </a>
-          </div>
-          <button class="navbar-toggler border border-light" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <i class="fas fa-bars text-light"></i>
-          </button>
-          <div class="collapse navbar-collapse d-xl-flex justify-content-end" id="navbarNav">
-            <ul class="navbar-nav">
-              @guest
-                  <li class="nav-item">
-                      <a class="nav-link link-light" href="{{ route('login') }}">{{ __('Login') }}</a>
-                  </li>
-                  @if (Route::has('register'))
-                      <li class="nav-item">
-                          <a class="nav-link link-light" href="{{ route('register') }}">{{ __('Register') }}</a>
-                      </li>
-                  @endif
-              @else
-              <li class="nav-item dropdown">
-                  <a id="navbarDropdown" class="nav-link dropdown-toggle link-light" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                      {{ Auth::user()->name }}
-                  </a>
-        
-                  <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                      <a class="dropdown-item" href="{{ route('admin.animals.index') }}">Dashboard</a>
-                      <a class="dropdown-item" href="{{ url('profile') }}">{{__('Profile')}}</a>
-                      <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
-                                           document.getElementById('logout-form').submit();">
-                          {{ __('Logout') }}
-                      </a>
-        
-                      <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                          <a class="dropdown-item" href="{{ route('admin.animals.index') }}">dashboard</a>
-                          <a class="dropdown-item" href="{{ url('profile') }}">{{ __('Profile') }}</a>
-                          <a class="dropdown-item" href="{{ route('logout') }}"
-                              onclick="event.preventDefault();
-                                           document.getElementById('logout-form').submit();">
-                              {{ __('Logout') }}
-                          </a>
-        
-                          <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                              @csrf
-                          </form>
-                      </div>
-                  </li>
-              @endguest
-            </ul>
-          </div>
+    <div class="container-fluid container">
+        <div class="col-lg-2 col-3 col-xl-1">
+            <a class="navbar-brand" href="{{ url('/') }}">
+                <img src="{{ Vite::asset('public\img\BoolPetCare@4x.png')}}" alt=""  id="logo">
+            </a>
         </div>
-    </nav>
-        </nav>
+        @auth
+            <!-- Visualizza solo quando l'utente è autenticato -->
+             <div class="d-xl-flex justify-content-end pt-3" id="navbarNav">
+                <ul class="">
+                    <li class="nav-item dropdown">
+                        <a id="navbarDropdown" class="nav-link dropdown-toggle link-light" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                            {{ Auth::user()->name }}
+                        </a>
+                        <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                            <a class="dropdown-item" href="{{ route('admin.animals.index') }}">Dashboard</a>
+                            <a class="dropdown-item" href="{{ url('profile') }}">{{__('Profile')}}</a>
+                            <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                                {{ __('Logout') }}
+                            </a>
+                            <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                @csrf
+                            </form>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+        @endauth
+        @guest
+            <!-- Visualizza solo quando l'utente è un ospite -->
+            <button class="navbar-toggler border border-light" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <i class="fas fa-bars text-light"></i>
+            </button>
+            <div class="collapse navbar-collapse d-xl-flex justify-content-end" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link link-light" href="{{ route('login') }}">{{ __('Login') }}</a>
+                    </li>
+                    @if (Route::has('register'))
+                        <li class="nav-item">
+                            <a class="nav-link link-light" href="{{ route('register') }}">{{ __('Register') }}</a>
+                        </li>
+                    @endif
+                </ul>
+            </div>
+        @endguest
+    </div>
+</nav>
 
         <main>
             <div class="container-fluid">
